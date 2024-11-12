1e4ea15d6fb19f47da09bcd9d2e841563aebcd1f
XintaoYu <2019202446@ruc.edu.cn>
2024-11-12 21:38:56 +0800

    lake build

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "PizzaGameLean4" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «PizzaGameLean4» where
+  -- add any library configuration options here
