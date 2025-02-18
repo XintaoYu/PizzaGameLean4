5b560da57dab30c8a92509b2095ea92324da4f17
XintaoYu <1332154797@qq.com>
2025-02-18 21:05:41 +0800

    Update Basic.lean

--- PizzaGameLean4/Basic.lean
+++ PizzaGameLean4/Basic.lean
@@ -11,7 +11,7 @@ instance Fin.instCircularOrder : CircularOrder (Fin (n + 1)) where
   sbtw := fun a b c =>
     if a < c then a < b ∧ b < c
     else if c < a then a < b ∨ b < c
-    else True
+    else False
   btw_refl := fun _ => by simp
   btw_cyclic_left := @fun a b c habc => by
     simp [btw] at *
@@ -45,12 +45,72 @@ instance Fin.instCircularOrder : CircularOrder (Fin (n + 1)) where
       · simp [btw, sbtw] at *
         split at h
         · next h₁ =>
-
-          sorry
+          simp [h₁]
+          exact ⟨le_of_lt h.1, le_of_lt h.2⟩
         · next h₁ =>
-          sorry
-      · sorry
-    · sorry
-  sbtw_trans_left := sorry
+          simp [h₁]
+          intro this
+          rcases h.2 with h | h
+          · left
+            exact le_of_lt h
+          · right
+            exact le_of_lt h
+      · simp only [btw, sbtw] at *
+        split at h
+        · next h₁ =>
+          have : ¬ (c ≤ b ∨ b ≤ a) := by
+            simp
+            exact ⟨h.2, h.1⟩
+          simp [h₁, this]
+          intro h' h₁'
+          exact h.1
+        · next h₁ =>
+          have := le_of_not_lt h₁
+          rcases lt_or_eq_of_le this with hd | hd
+          · simp [hd] at *
+            intro h'
+            rcases h with h | h
+            · exact h
+            · absurd h
+              exact not_lt_of_le h'
+          · simp [hd] at *
+    · intro h
+      simp only [btw, sbtw] at *
+      rcases h with ⟨h₁, h₂⟩
+      split at h₁
+      · next hd =>
+        have hd' : ¬ c < a := not_lt_of_lt hd
+        simp [hd, hd'] at *
+        exact ⟨h₂.2, h₂.1⟩
+      · next hd =>
+        have := le_of_not_lt hd
+        rcases lt_or_eq_of_le this with hd | hd
+        · have hd' := not_lt_of_lt hd
+          simp [hd, hd'] at *
+          rcases h₁ with h₁' | h₁'
+          · rcases lt_or_eq_of_le h₁' with h₁' | h₁'
+            · left
+              exact h₁'
+            · rw [h₁'] at this
+              left
+              exact h₂ this
+          · rcases lt_or_eq_of_le h₁' with h₁' | h₁'
+            · right
+              exact h₁'
+            · left
+              exact h₂ (le_of_eq h₁'.symm)
+        · simp [hd] at *
+  sbtw_trans_left := @fun a b c d fabc fbdc => by
+    simp only [sbtw] at *
+    split at fabc
+    · next h =>
+      simp [h]
+      split at fbdc
+      · next h' =>
+        exact ⟨lt_trans fabc.1 fbdc.1, fbdc.2⟩
+      · next h' =>
+        sorry
+    · next h =>
+      sorry
   btw_antisymm := sorry
   btw_total := sorry
