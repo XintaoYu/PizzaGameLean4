996c17f7cf05b5ab0003170a42aa6eba8ac00dca
XintaoYu <1332154797@qq.com>
2025-02-18 18:21:50 +0800

    Update Basic.lean

--- PizzaGameLean4/Basic.lean
+++ PizzaGameLean4/Basic.lean
@@ -1,1 +1,12 @@
-def hello := "world"
+import Mathlib
+
+--NOTE : it seems that we can replace n with n + 1 and NeZero n ; it is equivalent to n > 1 and there will be much lesser cases to deal with
+variable {n : ℕ} [NeZero n]
+
+def 
+
+structure game where
+  toFun : Fin (n + 1) → Fin (n + 1)
+  rule : ∀ (i : Fin (n + 1)) (h : 0 < i.val), toFun i ∈
+
+example : 1 + 1 = 2 := rfl
